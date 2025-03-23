69acf6cc31540db822c73a6244fe749055b86151
Sputnikjoa <joaquin.ros.sed15@gmail.com>
2025-03-23 16:35:21 -0600

    cambio de url

--- infoescom-frontend/src/pages/ResetPasswordPage.tsx
+++ infoescom-frontend/src/pages/ResetPasswordPage.tsx
@@ -57,7 +57,7 @@ function ResetPasswordPage() {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/users/reset-password', {
+      const response = await fetch('https://infoescom.site/api/users/reset-password', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ token, newPassword: password }), // Asegúrate de enviar "newPassword"
