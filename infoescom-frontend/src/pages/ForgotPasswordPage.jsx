a618bbf6b31c44ab3d52edc5d6e95388bc8f24cd
Sputnikjoa <joaquin.ros.sed15@gmail.com>
2025-03-23 16:31:28 -0600

    cambio de url

--- infoescom-frontend/src/pages/ForgotPasswordPage.jsx
+++ infoescom-frontend/src/pages/ForgotPasswordPage.jsx
@@ -24,7 +24,7 @@ function ForgotPasswordPage() {
     setMessage('');
     setError('');
     try {
-      const response = await fetch('http://localhost:5000/api/users/forgot-password', {
+      const response = await fetch('https://infoescom.site/api/users/forgot-password', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email })
